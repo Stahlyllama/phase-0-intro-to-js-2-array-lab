d95e47deb604f7d99367afe842e413db8fa816b5
Kim Stahl <kim.stahl@gmail.com>
2023-07-26 04:24:07 -0500

    cleaner version of arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+  cats.push("Ralph");  
+}
+
+function destructivelyRemoveLastCat(){
+ cats.pop(); 
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift ("Bob");
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+
+function appendCat(name){
+  appendCat = cats.concat("Broom");  
+ return appendCat;
+}
+
+function prependCat(name){
+  prependCat = ["Arnold"].concat(cats);
+  return prependCat;
+}
+
+function removeLastCat(){
+ removeLastCat = cats.slice(0,-1);
+ return removeLastCat;
+}
+
+function removeFirstCat(){
+ removeFirstCat = cats.slice(1);
+ return removeFirstCat;
+}
+
+
+
+
+
+
+
+  
+
+
+
+
